e6af0ff355e9b05f2b7ceb8101bcd48389c49c9b
Filipenstrom <filip.enstrom@gmail.com>
2018-03-05 10:19:51 +0100

    skapat databasklass

---
+++ app/src/main/java/com/example/filip/unibook/DatabaseHelper.java
@@ -0,0 +1,25 @@
+package com.example.filip.unibook;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by filip on 2018-03-05.
+ */
+
+public class DatabaseHelper extends SQLiteOpenHelper{
+    public DatabaseHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
