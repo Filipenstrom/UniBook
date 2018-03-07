58bba525c2d0bb04e9753e50bbe9df5ce08e1dc3
LudvigHeine <ludvig.heine91@gmail.com>
2018-03-07 11:23:32 +0100

    Fix

--- app/src/main/java/com/example/filip/unibook/EditProfileActivity.java
+++ app/src/main/java/com/example/filip/unibook/EditProfileActivity.java
@@ -2,12 +2,23 @@ package com.example.filip.unibook;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
 public class EditProfileActivity extends AppCompatActivity {
+
+    EditText editName, editSurname, editEmail, editPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+        editName = (EditText) findViewById(R.id.editTxtName);
+        editSurname = (EditText) findViewById(R.id.editTxtSurname);
+        editEmail = (EditText) findViewById(R.id.editTxtMail);
+        editPassword = (EditText) findViewById(R.id.editTxtPass);
+        imageView = (ImageView) findViewById(R.id.ivProfile);
     }
 }
