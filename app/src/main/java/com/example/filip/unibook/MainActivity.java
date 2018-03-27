30e9f449e5304b87aa7d2ae092a2cdab5b1f1452
markusdanielsson <markus_danielsson@live.se>
2018-03-27 17:30:53 +0200

    mm

--- app/src/main/java/com/example/filip/unibook/MainActivity.java
+++ app/src/main/java/com/example/filip/unibook/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -18,64 +19,35 @@ import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
-    DatabaseHelper myDb;
-    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
-    Button login = (Button) findViewById(R.id.loginBtn);
-    EditText email = (EditText) findViewById(R.id.editTxtUsername);
-    EditText password = (EditText) findViewById(R.id.editTxtPassword);
+    private FirebaseAuth mAuth;
+    Button loginBtn, registerBtn;
+    EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        myDb = new DatabaseHelper(this);
+        mAuth = FirebaseAuth.getInstance();
+        loginBtn = findViewById(R.id.loginBtn);
+        registerBtn = findViewById(R.id.registreraBtn);
+        email = findViewById(R.id.editTxtUsername);
+        password = findViewById(R.id.editTxtPassword);
 
-
-        //EditText username = (EditText) findViewById(R.id.editTxtUsername);
-        //EditText password = (EditText) findViewById(R.id.editTxtPassword);
-
-        Button registerBtn = (Button) findViewById(R.id.registreraBtn);
-
-        registerBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent regIntent = new Intent(MainActivity.this, RegisterActivity.class);
-                startActivity(regIntent);
-            }
-        });
-
-        login.setOnClickListener(new View.OnClickListener() {
+        loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 logIn();
             }
         });
 
-
-
-        //login.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View v) {
-        //        EditText username = (EditText) findViewById(R.id.editTxtUsername);
-        //        EditText password = (EditText) findViewById(R.id.editTxtPassword);
-        //        String user = username.getText().toString();
-        //        String losenord = password.getText().toString();
-//
-        //        String pass = myDb.searchPass(user);
-        //        if(losenord.equals(pass)){
-//
-        //            Intent loggedIn = new Intent(MainActivity.this, LoggedInActivity.class);
-        //
-        //            //Kör metoden för att spara ner username.
-        //            saveUserInformation();
-        //            startActivity(loggedIn);
-        //        }
-        //        else {
-        //            Toast.makeText(MainActivity.this,"Username och password don't match!", Toast.LENGTH_LONG).show();
-        //        }
-        //    }
-        //});
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(MainActivity.this, RegisterActivity.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 
     public void logIn(){
@@ -86,30 +58,30 @@ public class MainActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
-                            //Log.d(TAG, "signInWithEmail:success");
+                            Log.d("message", "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            //updateUI(user);
 
-                            Intent regIntent = new Intent(MainActivity.this,LoggedInActivity.class);
-                            startActivity(regIntent);
+                            Intent logInIntent = new Intent(MainActivity.this, LoggedInActivity.class);
+                            startActivity(logInIntent);
+                            //updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
-                            //Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Log.w("message", "signInWithEmail:failure", task.getException());
                             Toast.makeText(MainActivity.this, "Authentication failed.",
                                     Toast.LENGTH_SHORT).show();
                             //updateUI(null);
                         }
-
                         // ...
                     }
                 });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
 
-    //Kod för att spara ner username så man kan nå det i alla aktiviteter.
-    public void saveUserInformation(){
-        EditText username = (EditText) findViewById(R.id.editTxtUsername);
-        SharedPreferences sp = new SharedPreferences(this);
-        sp.setusername(username.getText().toString());
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
     }
 }
