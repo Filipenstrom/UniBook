e5941f199cf7b6d8973112e63720219071148b96
markusdanielsson <markus_danielsson@live.se>
2018-05-16 09:52:53 +0200

    mm

--- app/src/main/java/com/example/filip/unibook/SettingsActivity.java
+++ app/src/main/java/com/example/filip/unibook/SettingsActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SettingsActivity extends AppCompatActivity {
     Context context = this;
 
