9fc6cce6d382d475021f636bc09083274885fbdd
Filipenstrom <filip.enstrom@gmail.com>
2018-05-16 10:14:20 +0200

    lol

--- app/src/main/java/com/example/filip/unibook/SettingsActivity.java
+++ app/src/main/java/com/example/filip/unibook/SettingsActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -15,39 +16,48 @@ import com.google.firebase.auth.FirebaseUser;
 public class SettingsActivity extends AppCompatActivity {
     Context context = this;
     FirebaseAuth mAuth = FirebaseAuth.getInstance();
-    FirebaseUser user = mAuth.getCurrentUser();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-        logOut();
-        goToNotiser();
-    }
 
-    //Metod för att logga ut
-    public void logOut(){
+
         Button btnlogout =  findViewById(R.id.btnlogOut);
         btnlogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mAuth.signOut();
-                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                logOut();
+
             }
         });
-    }
 
-    //Metod för att gå till notiser
-    public void goToNotiser(){
         Button btnnotiser =  findViewById(R.id.btnnotiser);
         btnnotiser.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SettingsActivity.this, NotificationActivity.class);
-                startActivity(intent);
+                goToNotiser();
             }
         });
+
+    }
+
+
+
+    //Metod för att logga ut
+    public void logOut(){
+                mAuth.signOut();
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+
+    }
+
+    //Metod för att gå till notiser
+    public void goToNotiser(){
+                Intent intent = new Intent(SettingsActivity.this, NotificationActivity.class);
+                startActivity(intent);
+
     }
 }
