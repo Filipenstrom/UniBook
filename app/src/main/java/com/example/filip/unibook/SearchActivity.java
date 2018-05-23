30eea7d5e1680a8d1cf31c3acf4e87280d4cdc53
markusdanielsson <markus_danielsson@live.se>
2018-05-23 10:39:40 +0200

    mm

--- app/src/main/java/com/example/filip/unibook/SearchActivity.java
+++ app/src/main/java/com/example/filip/unibook/SearchActivity.java
@@ -173,21 +173,15 @@ public class SearchActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
                 Intent chosenAdForSale = new Intent(context, ChosenAdForSaleActivity.class);
-                ImageView adPic = view.findViewById(R.id.ivAdsListPicture);
+                //ImageView adPic = view.findViewById(R.id.ivAdsListPicture);
                 TextView id = view.findViewById(R.id.txtAdID);
 
-                try{
 
-                    Bitmap bitmap = ((BitmapDrawable)adPic.getDrawable()).getBitmap();
-                    chosenAdForSale.putExtra("img", bitmap);
+                    //Bitmap bitmap = ((BitmapDrawable)adPic.getDrawable()).getBitmap();
+                    //chosenAdForSale.putExtra("img", bitmap);
                     chosenAdForSale.putExtra("id", id.getText().toString());
                     startActivity(chosenAdForSale);
 
-                }catch(Exception e){
-
-                    Log.d("Error", e.getMessage().toString());
-
-                }
             }
         });
 
